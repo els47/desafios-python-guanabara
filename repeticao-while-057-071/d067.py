fe109b67b1854b5ad017362ca38a7f5ca456b42d
Eduarda Soares <dudasoa23@gmail.com>
2025-11-15 14:17:08 -0300

    exercises update

--- repeticao-while-057-071/d067.py
+++ repeticao-while-057-071/d067.py
@@ -1,1 +1,11 @@
 # Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. O programa será interrompido quando o número solicitado for negativo.
+
+n = c = 0
+
+while True:
+    n = int(input('Digite um número inteiro: '))
+    if n < 0:
+        break
+    for i in range (0, 11):
+        print(f'{n} x {i} = {n*i}', end = ' | ')
+print('Número inválido. Programa encerrado!')
